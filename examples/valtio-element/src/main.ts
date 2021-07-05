a16df34f7748374655b9d820b2dde1e59f77aa78
Alexis Munsayac <alexis.munsayac@gmail.com>
2021-07-05 23:01:56 +0800

    Update main.ts

--- examples/valtio-element/src/main.ts
+++ examples/valtio-element/src/main.ts
@@ -1,4 +1,4 @@
-import { proxy } from 'valtio';
+import { proxy } from 'valtio/vanilla';
 import { watch } from 'valtio/utils';
 import { setRenderer, define } from 'valtio-element';
 import { render, html } from 'lit-html';
